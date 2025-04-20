071091672f1931ea6348654057a508043a6dc160
Andre <andre.acero@cetysuniversidad.edu.mx>
2025-04-20 11:24:36 -0700

    Reiniciar, Cambio en tailwind.config

--- frontend/components/LoginService.ts
+++
@@ -1,9 +0,0 @@
-// components/LoginService.ts
-import ApiService from "../services/ApiService";
-const api = ApiService.getInstance();
-
-export const loginAPI = (username: string, password: string) =>
-  api.login(username, password);
-
-export const registerAPI = (email: string, username: string, password: string) =>
-  api.register(email, username, password);
